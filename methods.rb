d340a0c734232d6f22c0862a559748e08576e727
rickie <rickiemaweu@gmail.com>
2022-08-16 19:50:02 +0300

    done

--- methods.rb
+++ methods.rb
@@ -1,1 +1,25 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+  end
+  
+  def greet(name)
+    puts "Hello, #{name}!"
+  end
+  
+  def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+  
+  def add (a, b)
+    a + b
+  end
+  
+  def halve (a)
+    if a.is_a?(Integer)
+      a / 2
+    else
+      puts "Not an integer"
+    end
+  end
+   
